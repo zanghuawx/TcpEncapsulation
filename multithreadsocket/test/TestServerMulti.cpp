b8f62212bcbc0ad71d3033e311dc168b2edfa22e
wuxiao <601798312@qq.com>
2019-05-06 15:45:53 +0800

    multi server

--- multithreadsocket/test/TestServerMulti.cpp
+++ multithreadsocket/test/TestServerMulti.cpp
@@ -1,11 +1,13 @@
-#include "TestAll.h"
+#include "TestServerMulti.h"
 
 
-TestServerMulti::TestServerMulti(EventLoop * loop, const int port, const int threadNum) 
-	: server_(loop, port, threadNum) {
-	server_.setConnectionCallback(std::bind(&TestAll::onConnection, this, std::placeholders::_1));
-	server_.setMessageCallback(std::bind(&TestAll::onMessage, this, std::placeholders::_1, std::placeholders::_2));
-	server_.setSendCompleteCallback(std::bind(&TestAll::onSendComplete, this, std::placeholders::_1));
+TestServerMulti::TestServerMulti(EventLoop * loop, const int port, const int threadNum, const int threadPoolNum) 
+	: server_(loop, port, threadNum), pool_(threadPoolNum) {
+	server_.setConnectionCallback(std::bind(&TestServerMulti::onConnection, this, std::placeholders::_1));
+	server_.setMessageCallback(std::bind(&TestServerMulti::onMessage, this, std::placeholders::_1, std::placeholders::_2));
+	server_.setSendCompleteCallback(std::bind(&TestServerMulti::onSendComplete, this, std::placeholders::_1));
+
+	pool_.start();
 }
 
 TestServerMulti::~TestServerMulti() {
@@ -36,11 +38,16 @@ void TestServerMulti::sendMessage(const TcpConnectionPtr& conn, const std::strin
 	//遍历连接红黑树,把消息发送到每一个tcp中
 	for (std::map<int, TcpConnectionPtr>::iterator it = server_.tcpConnList_.begin(); it != server_.tcpConnList_.end(); ++it) {
 		//std::cout << "socketfd: " << it->first << std::endl;	//it->first 是红黑树的关键字
-		it->second->sendString(message);	//it->second 是红黑树的值
+		//it->second->sendString(message);	//it->second 是红黑树的值
+		ThreadPool::Task func = std::bind(&TestServerMulti::sendInPool, this, it->second, message);
+		pool_.addTask(func);
 	}
-
-	//conn->sendString(message);
 }
+
+void TestServerMulti::sendInPool(const TcpConnectionPtr& conn, const std::string& message) {
+	conn->sendString(message);
+}
+
 
 void TestServerMulti::onSendComplete(const TcpConnectionPtr& conn) {
 	std::cout << "send complete!" << std::endl;
--- multithreadsocket/test/TestServerMulti.h
+++ multithreadsocket/test/TestServerMulti.h
@@ -3,21 +3,26 @@
 #define _TESTSERVERMULTI_H_
 
 #include "TcpServer.h"
+#include "ThreadPool.h"
+#include <unistd.h>
+
 
 class TestServerMulti {
 public:
 	typedef std::shared_ptr<TcpConnection> TcpConnectionPtr;
 	typedef std::function<void (const TcpConnectionPtr&, std::string&)> MessageCallback;
 	typedef std::function<void (const TcpConnectionPtr&)> ConnectionCallback;
-	TestAll(EventLoop * loop, const int port, const int threadNum = 1);
-	~TestAll();
+	TestServerMulti(EventLoop * loop, const int port, const int threadNum = 5, const int threadPoolNum = 5);
+	~TestServerMulti();
 	void start();
 	void onConnection(const TcpConnectionPtr& conn);
 	void onMessage(const TcpConnectionPtr& conn, std::string& message);
 	void sendMessage(const TcpConnectionPtr& conn, const std::string& message);
+	void sendInPool(const TcpConnectionPtr& conn, const std::string& message);
 	void onSendComplete(const TcpConnectionPtr& conn);
 private:
 	TcpServer server_;
+	ThreadPool pool_;
 
 	
 };
